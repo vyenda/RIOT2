399515a2408bdf32148ad345f06d48c18d31f5c5
hastrong <148916556+hastrong@users.noreply.github.com>
2023-11-16 14:55:50 -0800

    Player and enemy scripts
    I've coded player health and movement and am starting on the first enemy.

---
+++ RIOT/Assets/Scripts/EnemyL1D1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author(s): [Strong, Hannah]; [Arellano, Angeleen]
+ * Date Last Modified: [11/16/2023]
+ * Codes for the first level, first difficulty enemy
+ */
+
+public class EnemyL1D1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RIOT/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author(s): [Strong, Hannah]; [Arellano, Angeleen]
+ * Date Last Modified: [11/16/2023]
+ * Codes for the player movement
+ */
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigidbodyRef;
+
+    public float speed = 1f;
+    private Vector3 startPos;
+    public float jumpForce = 10f;
+    public float health = 100f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //stores the rigidbody of the player as a reference
+        rigidbodyRef = GetComponent<Rigidbody>();
+
+        //stores the players current position in the level
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if A is pressed
+        if (Input.GetKey(KeyCode.A))
+        {
+            //the player will move left
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        //if D is pressed
+        if (Input.GetKey(KeyCode.D))
+        {
+            //the player will move right
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        //if space bar is pressed
+        if (Input.GetKey(KeyCode.Space))
+        {
+            HandleJump();
+        }
+
+        //tracks the player's health to see if they are dead or not
+        GameOver();
+    }
+
+    /// <summary>
+    /// codes for the player jumping; they have to touch the ground before jumping
+    /// </summary>
+    private void HandleJump()
+    {
+        //Raycasts from the bottom of the player and detects if the ground is there
+        RaycastHit hit;
+
+        //if the ground is there
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.3f))
+        {
+            //they can jump
+            rigidbodyRef.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            Debug.Log("The player can jump.");
+        }
+        else
+        {
+            //if the ground is not there, then they can't
+            Debug.Log("The player can't jump.");
+        }
+    }
+
+    /// <summary>
+    /// if the player's health reaches 0 or below, the game will end
+    /// </summary>
+    private void GameOver()
+    {
+        if (health <= 0)
+        {
+            //add a scene transition here when UI has been created
+            Debug.Log("The player died. Game over.");
+        }
+    }
+}
