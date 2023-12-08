99e0fcd9eabf8d8dceae097529f88e041a2a1b0c
vyenda <angeleenarellano@gmail.com>
2023-12-07 20:51:36 -0800

    Removed Enemy health UI

--- RIOT/Assets/Scripts/UIManager.cs
+++ RIOT/Assets/Scripts/UIManager.cs
@@ -14,15 +14,6 @@ public class UIManager : MonoBehaviour
 {
     public PlayerController playerController;
     public TMP_Text playerHealth;
-
-    public TMP_Text easyE1H;
-    public EnemyL1D1 enemyL1D1;
-
-    public TMP_Text easyE3H;
-    public EnemyL3D1 enemyL3D1;
-
-    public TMP_Text easyE2H;
-    public EnemyL1D2 enemyL1D2;
     
     // Start is called before the first frame update
     void Start()
@@ -34,8 +25,5 @@ public class UIManager : MonoBehaviour
     void Update() 
     {
         playerHealth.text = "Player HP: " + playerController.healthPoints.ToString();
-        easyE1H.text = "Enemy 1 HP: " + enemyL1D1.health.ToString();
-        easyE2H.text = "Enemy 3 HP: " + enemyL1D2.health.ToString();
-        easyE3H.text = "Enemy 2 HP: " + enemyL3D1.health.ToString();
     }
 }
